738a20d477fa67b000a4d85795cd0a2bbd7d94db
artur nista <arturnista@gmail.com>
2019-01-26 00:17:59 -0200

    Player basic movement

---
+++ Assets/Player/InputSchema/PlayerInputSchema.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Create PlayerInputSchema")]
+public class PlayerInputSchema : ScriptableObject {
+
+    public KeyCode Right = KeyCode.D;
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Jump = KeyCode.Space;
+    public KeyCode Pickup = KeyCode.E;
+
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
+    private PlayerInputSchema inputSchema;
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
     private float moveSpeed;
     [SerializeField]
     private float jumpHeight;
 
+    private Vector2 desirableVelocity;
+    private Vector2 moveVelocity;
+    private Vector2 velocity;
+
+    private float gravity;
+
+    private bool isGrounded;
+
     private Rigidbody2D rigidbody;
 
     void Awake () {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D> ();
+        gravity = -Physics2D.gravity.y;
     }
 
     void Update () {
 
+        float horizontal = 0f;
+        if (Input.GetKey (inputSchema.Right)) horizontal = 1f;
+        else if (Input.GetKey (inputSchema.Left)) horizontal = -1f;
+
+        desirableVelocity.x = horizontal * moveSpeed;
+
+        moveVelocity.x = Mathf.MoveTowards (velocity.x, desirableVelocity.x, acceleration);
+
+        isGrounded = Physics2D.OverlapCircle (transform.position, .51f, 1 << LayerMask.NameToLayer ("Ground")) != null;
+        if (isGrounded) {
+            moveVelocity.y = 0f;
+            if (Input.GetKeyDown (inputSchema.Jump)) moveVelocity.y = Mathf.Sqrt (jumpHeight * gravity * 2f);
+        } else {
+            moveVelocity.y -= gravity * Time.deltaTime;
+        }
+
+    }
+
+    void LateUpdate () {
+        rigidbody.velocity = moveVelocity;
     }
 }
