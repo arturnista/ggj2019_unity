a2448d5766070ecb723ca446ce733651dcd3f250
artur nista <arturnista@gmail.com>
2019-01-25 23:40:55 -0200

    Ground

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpHeight;
+
+    private Rigidbody2D rigidbody;
+
+    void Awake () {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+
+    }
+}
